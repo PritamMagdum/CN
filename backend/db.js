1d0a687571f057608812218ab5f287f18ce7a3da
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-06 16:42:00 +0530

    Connected MongoDB with mongoose

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://127.0.0.1:27017/MyDatabase?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const connectToMongo = () => {
+    mongoose
+        .connect(mongoURI)
+        .then(() => {
+            console.log("Connected to Mongo Successfully");
+        })
+        .catch((err) => {
+            console.log(`error: ${err}`);
+        });
+}
+
+module.exports = connectToMongo;
