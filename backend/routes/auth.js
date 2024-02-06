0b3407cf027b1a500e3b4d76e95eae3d79b8c8d3
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-06 21:42:10 +0530

    Validate user data using epress-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
-// Create a user using POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+// Create a user using POST "/api/auth/createuser". No login required
+router.post('/createuser',
+    [
+        body('name', 'Enter a valid name').isLength({ min: 3 }),
+        body('email', 'Enter a valid email').isEmail(),
+        body('password', 'Enter a valid password').isLength({ min: 8 })
+    ], async (req, res) => {
+        console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // check whether the user is already exists or not
+        let user = await User.findOne({ email: req.body.email });
+        console.log(user);
+        if (user) {
+            return res.status(400).json({ error: "Sorrry user is already created" });
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        res.json(user);
+
+    })
 
 module.exports = router
